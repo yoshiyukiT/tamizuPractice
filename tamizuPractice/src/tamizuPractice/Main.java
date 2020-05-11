ce7aec44ffda012b64e21e68a334cc31ef55cf19
yoshiyukiT <noxnoy-axnl@outlook.jp>
2020-05-11 20:06:32 +0900

    first commit

---
+++ tamizuPractice/src/tamizuPractice/Main.java
@@ -0,0 +1,26 @@
+package tamizuPractice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for(int i=1;i<=100;){
+			if(i%3==0 || i%5==0){
+				if(i%3==0 && i%5==0){
+					System.out.println("FizzBizz");
+				}
+				if(i%5!=0){
+					System.out.println("Fizz");
+				}
+				if(i%3!=0){
+					System.out.println("Bizz");
+				}
+			}else{
+			System.out.println(i);
+			}
+			i++;
+		}
+
+	}
+
+}
