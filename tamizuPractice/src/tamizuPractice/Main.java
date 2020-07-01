ee9be35e5014e075f31ca51270c5df71df1f878a
yoshiyukiT <noxnoy-axnl@outlook.jp>
2020-07-02 07:55:24 +0900

    second commit

--- tamizuPractice/src/tamizuPractice/Main.java
+++ tamizuPractice/src/tamizuPractice/Main.java
@@ -20,7 +20,35 @@ public class Main {
 			}
 			i++;
 		}
+		String[] sweets = {"きたあああああ", "あああ", "い-------", "a111111", "aiiiiii", "", "1111112","111111","123456"};
+
+		for (int i = 0; i < sweets.length; i++) {
+		System.out.println(sweets[i] + ":: " +
+		sweets[i].matches("(.)\\1{" + 5 + ",}"));
+		}
+
+		int num = 5;
+
+		for (int i = 0; i < sweets.length; i++) {
+		System.out.println(sweets[i] + "::: " +
+		sweets[i].matches(".*(.)\\1{" + num + ",}"));
+		}
+
+		String password="aaaaaa";
+		String mailAddress="aaaaaa@gmail.com";
+
+		check(password,mailAddress,num);
 
 	}
 
+	private static boolean check(String str1,String str2,int num) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		if(str1.matches(".*(.)\\1{" + num + ",}") && str2.contains(str1)) {
+			System.out.println("true");
+		}else{
+			System.out.println("false");
+		};
+		return true;
+	}
 }
